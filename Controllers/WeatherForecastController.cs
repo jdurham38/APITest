1c52d95302cb9e2ed5b32c42e077a9cc871a5f48
Joshua Durham <jdurham9725@eagle.fgcu.edu>
2023-08-07 18:24:30 -0400

    edit api for reminder app

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,14 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
-namespace Reminder.Controllers{
+namespace Reminder.Controllers
+{
     [ApiController]
     [Route("api/[controller]")]
-    public class HelloWorldController : ControllerBase
+    public class ReminderController : ControllerBase
     {
+        private static List<string> reminders = new List<string>();
+
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult GetReminders()
         {
-            return Ok("Hello, world!");
+            return Ok(reminders);
+        }
+
+        [HttpPost]
+        public IActionResult AddReminder([FromBody] string reminder)
+        {
+            if (!string.IsNullOrWhiteSpace(reminder))
+            {
+                reminders.Add(reminder);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Invalid reminder");
+            }
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult DeleteReminder(int index)
+        {
+            if (index >= 0 && index < reminders.Count)
+            {
+                reminders.RemoveAt(index);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
