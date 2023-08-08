473f0cf72561732e46d20e0f636f973776151a29
Joshua Durham <jdurham9725@eagle.fgcu.edu>
2023-08-07 21:07:33 -0400

    Updated api to allow production site for cors

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-    builder.Services.AddCors(options =>
-    {
-        options.AddPolicy("AllowLocalhost",
-            builder =>
-            {
-                builder.WithOrigins("http://localhost:3000")
-                       .AllowAnyHeader()
-                       .AllowAnyMethod();
-            });
-    });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000", "https://digital-portfolio-josh.vercel.app")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
